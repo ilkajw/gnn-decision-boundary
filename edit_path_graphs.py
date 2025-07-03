3cca6ce18907530cf0df117457081af03b3d1e29
ilkajw <s6ilwull@uni-bonn.de>
2025-07-03 09:46:33 +0200

    changed filter logic to keep edit step meta data correct

--- edit_path_graphs.py
+++ edit_path_graphs.py
@@ -12,8 +12,11 @@ def generate_and_save_all_graphs(db_name="MUTAG",
                                  output_dir="data/pyg_edit_path_graphs",
                                  fully_connected=True):
     """
-    Generates and saves all intermediate PyG graphs from stored edit paths.
-    Optionally filters only fully connected graphs.
+    Generates all nx graphs from edit path sequences.
+    Optionally filters for fully connected graphs only.
+    Adds metadata for later filtering.
+    Converts to pyg objects.
+    Saves each pyg graph sequence identified by source graph, target graph, iteration to one .pt file.
 
     Args:
         db_name (str): Dataset name.
@@ -36,27 +39,37 @@ def generate_and_save_all_graphs(db_name="MUTAG",
     os.makedirs(output_dir, exist_ok=True)
 
     for (i, j), paths in edit_paths.items():
+
+        # loop through all paths between i, j
         for ep in paths:
+
+            # create edit path graph sequence for path 'ep' between i, j
             sequence = ep.create_edit_path_graphs(nx_graphs[i], nx_graphs[j], seed=seed)
 
-            if fully_connected:
-                sequence = [g for g in sequence if nx.is_connected(g)]
-
-            # convert to pyg with metadata
-            pyg_sequence = []
+            # assign metadata to nx graphs
             for step, g in enumerate(sequence):
                 g.graph['edit_step'] = step
                 g.graph['source_idx'] = i
                 g.graph['target_idx'] = j
                 g.graph['iteration'] = ep.iteration
                 g.graph['distance'] = ep.distance
+
+            # filter for fully connected graphs
+            if fully_connected:
+                sequence = [g for g in sequence if nx.is_connected(g)]
+
+            # convert to pyg with metadata
+            pyg_sequence = []
+
+            # convert nx objects to pyg and copy metadata
+            for step, g in enumerate(sequence):
                 pyg_g = from_networkx(g)
                 for meta_key, meta_val in g.graph.items():
                     setattr(pyg_g, meta_key, meta_val)
                 pyg_sequence.append(pyg_g)
 
             # save sequence to file
-            file_path = os.path.join(output_dir, f"g{i}_to_g{j}_it{ep.iteration}_graph_sequence_graph.pt")
+            file_path = os.path.join(output_dir, f"g{i}_to_g{j}_it{ep.iteration}_graph_sequence.pt")
             torch.save(pyg_sequence, file_path)
 
 
