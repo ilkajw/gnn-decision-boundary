fixed bug in accuracy calculation (sigmoid was missing). changed all read/save paths to be dataset dependent. added functions to filter for correctly classified MUTAG graphs. added functions to retrieve sets of all MUTAG graph pairs of same/diff class. put back functionality of create_and_save_all_edit_path_graphs() to test pipeline, temporarily altered loop to consider only graph pairs (0,1) , (0,2) to not run into errors. current pipeline without errors

edit_path_graphs.py
@@ -14,11 +14,11 @@ from pg_gnn_edit_paths.utils.GraphLoader.GraphLoader import GraphDataset
 from torch_geometric.utils import from_networkx
 
 
-def generate_and_save_all_edit_path_graphs(db_name="MUTAG",
+def generate_and_save_all_edit_path_graphs(data_dir,
+                                           output_dir,
+                                           db_name="MUTAG",
                                            seed=42,
-                                           data_dir="data",
-                                           output_dir="data/pyg_edit_path_graphs",
-                                           fully_connected=True):
+                                           fully_connected_only=True):
     """
     Generates all nx graphs from edit path sequences.
     Optionally filters for fully connected graphs only.
@@ -31,7 +31,7 @@ def generate_and_save_all_edit_path_graphs(db_name="MUTAG",
         seed (int): Random seed for edit path generation.
         data_dir (str): Directory where edit paths are stored.
         output_dir (str): Where to save the PyG graphs.
-        fully_connected (bool): If True, filter only connected graphs.
+        fully_connected_only (bool): If True, filter only connected graphs.
     """
 
     os.makedirs(output_dir, exist_ok=True)
@@ -53,14 +53,19 @@ def generate_and_save_all_edit_path_graphs(db_name="MUTAG",
     num_node_classes = dataset.unique_node_labels
     print(f"Number of node classes, equivalent to size of original feature tensor: {num_node_classes} \n ")
 
-    # todo: for debugging only. delete afterwards
+    # todo: for debugging node label values only. delete afterwards
     counter = 0
     max_label = 0
 
     # create graphs from operations per (source graph, target graph)
     for (i, j), paths in edit_paths.items():
 
-        # todo: right now only 1 path per pair. if not potentially more for other datasets, delete inner loop
+        # todo: for logic testing purposes only. to prevent running into errors in label transformation from
+        #  graph pairs (0, 3) onwards
+        if i != 0 or j > 2:
+            continue
+
+        # todo: right now only 1 path per pair. if not more for other datasets, delete inner loop
         # loop through all paths between graph pairs i, j
         for ep in paths:
 
@@ -76,7 +81,7 @@ def generate_and_save_all_edit_path_graphs(db_name="MUTAG",
                 g.graph['distance'] = ep.distance
 
             # filter for fully connected graphs
-            if fully_connected:
+            if fully_connected_only:
                 sequence = [g for g in sequence if nx.is_connected(g)]
 
             # drop edge attrs, convert to pyg objects, copy metadata
@@ -84,11 +89,12 @@ def generate_and_save_all_edit_path_graphs(db_name="MUTAG",
 
             for step, g in enumerate(sequence):
 
-                # todo: alternatively copy edge attrs from g and transform back to vectors. else check for
-                #  any use of edge attrs later on
+                # todo: alternatively use edge attrs of g too. then the loop to copy from g to g_with no_edge_attrs
+                #  is obsolete. also need to transform edge labels back to vectors to avoid when handing edge
+                #  labels to model
+
                 # strip edge attributes as not used for learning
                 g_no_edge_attrs = nx.Graph()
-
 
                 # add nodes with attr tensor x reconstructed from nx graph's scalar primary_label
                 for n, d in g.nodes(data=True):
@@ -102,23 +108,25 @@ def generate_and_save_all_edit_path_graphs(db_name="MUTAG",
                         max_label = label
 
                     # todo: causes error as label >= num_node_classes for many nodes
-                    #d['x'] = F.one_hot(torch.tensor(label), num_classes=num_node_classes).float()
-                    #g_no_edge_attrs.add_node(n, **d)
+                    d['x'] = F.one_hot(torch.tensor(label), num_classes=num_node_classes).float()
+                    print(f"DEBUG: Transformed label {label} of node {n} graph {g.graph['edit_step']} between "
+                          f"{i}, {j} into {d['x']}")
+                    g_no_edge_attrs.add_node(n, **d)
 
                 # add edges without attributes
-                #g_no_edge_attrs.add_edges_from(g.edges())
+                g_no_edge_attrs.add_edges_from(g.edges())
 
                 # convert to PyG
-                #pyg_g = from_networkx(g_no_edge_attrs)
+                pyg_g = from_networkx(g_no_edge_attrs)
 
                 # copy metadata
-                #for meta_key, meta_val in g.graph.items():
-                #    setattr(pyg_g, meta_key, meta_val)
-                #pyg_sequence.append(pyg_g)
+                for meta_key, meta_val in g.graph.items():
+                    setattr(pyg_g, meta_key, meta_val)
+                pyg_sequence.append(pyg_g)
 
-    print(f"Number of nodes with label >= {num_node_classes}: {counter} \n Max label: {max_label}")
             # save sequence to file
-            #file_path = os.path.join(output_dir, f"g{i}_to_g{j}_it{ep.iteration}_graph_sequence.pt")
-            #torch.save(pyg_sequence, file_path)
+            file_path = os.path.join(output_dir, f"g{i}_to_g{j}_it{ep.iteration}_graph_sequence.pt")
+            torch.save(pyg_sequence, file_path)
+            print(f"Saved pyg sequence between {i}, {j}, iteration {ep.iteration}")
 
-
+    # print(f"Number of nodes with label >= {num_node_classes}: {counter} \n Max label: {max_label}")

evaluate.py
@@ -8,8 +8,9 @@ def evaluate_accuracy(model, loader, device):
     with torch.no_grad():
         for data in loader:
             data = data.to(device)
-            out = model(data.x, data.edge_index, data.batch).view(-1)
-            pred = (out > 0).long()  # todo: suitable criterion for 1 or 0?
+            out = model(data.x, data.edge_index, data.batch)
+            probs = torch.sigmoid(out.view(-1))
+            pred = (probs > 0.5).long()
             correct += (pred == data.y).sum().item()  # sum up correct predictions
     return correct / len(loader.dataset)  # return accuracy
 

model.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch_geometric.nn import GATv2Conv, global_add_pool
 
 
-# GAT model using GATv2 layers
+# GAT model using GATv2 layers for dynamic attention
 class GAT(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, heads=4, dropout=0.5):
         super().__init__()
@@ -17,16 +17,14 @@ class GAT(torch.nn.Module):
 
     def forward(self, x, edge_index, batch):
 
-        # todo: use elu like in original paper
-
-        # gatv2 layers with relu activation
-        x = F.relu(self.gat1(x, edge_index))
+        # GATv2 layers with relu activation
+        x = F.elu(self.gat1(x, edge_index))
         x = F.dropout(x, p=self.dropout, training=self.training)
-        x = F.relu(self.gat2(x, edge_index))
+        x = F.elu(self.gat2(x, edge_index))
         x = F.dropout(x, p=self.dropout, training=self.training)
-        x = F.relu(self.gat3(x, edge_index))
+        x = F.elu(self.gat3(x, edge_index))
         x = F.dropout(x, p=self.dropout, training=self.training)
-        x = F.relu(self.gat4(x, edge_index))
+        x = F.elu(self.gat4(x, edge_index))
         # readout: sum pooling
         x = global_add_pool(x, batch)
         return self.lin(x)

predict.py
@@ -9,13 +9,15 @@ from config import *
 from model import GAT
 
 
-def mutag_predictions(model_path="model/model.pt",
-                      output_path="data/predictions/mutag_predictions.json"):
+def dataset_predictions(dataset_name,
+                        output_dir,
+                        output_fname,
+                        model_path="model/model.pt"):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # load dataset
-    dataset = TUDataset(root=ROOT, name=DATASET_NAME)
+    dataset = TUDataset(root=ROOT, name=dataset_name)
     loader = DataLoader(dataset, batch_size=1, shuffle=False)  # batch_size=1 to keep index tracking
 
     # re-instantiate model
@@ -47,18 +49,19 @@ def mutag_predictions(model_path="model/model.pt",
         }
         # separate correctly classified graphs
         if correct:
-            # todo: potentially return lists. right now, nothing happens
+            # todo: potentially return lists. right now, nothing happens. define how logic works with indices
             if true == 0:
                 correct_class_0[idx] = predictions[idx]
             elif true == 1:
                 correct_class_1[idx] = predictions[idx]
 
-    os.makedirs("data/predictions", exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, output_fname)
     with open(output_path, "w") as f:
         json.dump(predictions, f, indent=2)
 
 
-def edit_path_predictions(model_path, input_dir, output_dir, dataset_name):
+def edit_path_predictions(dataset_name, model_path, input_dir, output_dir, output_fname):
     """
     Loads all pyg graph sequences, each indexed by (source, target graph, iteration).
     Runs predictions on all graphs per sequence.
@@ -86,7 +89,7 @@ def edit_path_predictions(model_path, input_dir, output_dir, dataset_name):
     model.eval()
 
     os.makedirs(output_dir, exist_ok=True)
-
+    os.makedirs(os.path.join(output_dir, "edit_path_graphs_with_predictions"), exist_ok=True)
     predictions = []
 
     # loop through ep graph sequences indexed by source, target, iteration
@@ -127,13 +130,18 @@ def edit_path_predictions(model_path, input_dir, output_dir, dataset_name):
                 "probability": prob
             })
 
-        # todo: rethink if putting pred to metadata and saving is necessary or if prediction dict is sufficient
-        torch.save(updated_sequence, os.path.join(output_dir, filename))
+        # todo: rethink if putting pred to metadata and saving is necessary or if saving prediction dict is sufficient
+        # save predictions as metadata for graphs
+        torch.save(updated_sequence, os.path.join(output_dir, "edit_path_graphs_with_predictions", filename))
+
+    # save predictions dict
+    with open(os.path.join(output_dir, output_fname), "w") as f:
+        json.dump(predictions, f, indent=2)
 
     return predictions
 
 
-def add_metadata_to_edit_path_predictions(pred_dict, base_pred_path, split_path, output_path):
+def add_metadata_to_edit_path_predictions(pred_dict_path, base_pred_path, split_path, output_path):
     """
     Enriches dictionary entries of edit path predictions with additional metadata to help later analysis:
     - true class labels of source and target
@@ -141,22 +149,25 @@ def add_metadata_to_edit_path_predictions(pred_dict, base_pred_path, split_path,
     - whether source/target were classified correctly
 
     Args:
+        :param pred_dict_path:
         :param split_path: Path to saved train, test split
         :param base_pred_path: Path to original MUTAG predictions file
-        :param pred_dict: Predictions (list): List of prediction dicts on edit path graphs
         :param output_path: Path to file where enriched dictionary is saved
 
     Returns:
         list: Enriched prediction dictionaries
     """
-
-    # load train, test split
-    with open(split_path, "r") as f:
-        split = json.load(f)
+    # load edit oath graph predictions
+    with open(pred_dict_path, "r", encoding="utf-8") as f:
+        pred_dict = json.load(f)
 
     # load predictions on org mutag graphs
     with open(base_pred_path, "r", encoding="utf-8") as f:
         base_preds = json.load(f)
+
+    # load train, test split
+    with open(split_path, "r") as f:
+        split = json.load(f)
 
     # add train vs. test split, classes of source & target, correct classification of source & train to metadata
     for entry in pred_dict: